15b79def7425258f14616545285b2184def6f4ee
Logon Aluno <logonrmlocal@fiap.com.br>
2019-01-24 23:17:56 -0200

    quarto commit

--- reactjs-projeto01/src/pages/Home/Index.jsx
+++ reactjs-projeto01/src/pages/Home/Index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
 class Home extends React.Component {
+
+    constructor() {
+        super();
+        this.search = this.search.bind(this);
+    }
+
+    search(event) {
+        console.log(event.currentTarget.value);
+    }
+
     render() {
-        return (<div>Home</div>);
+        return (
+    <div>
+        <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        <input onChange={this.search} className="mdl-textfield__input" type="text" id="search" />
+        <label className="mdl-textfield__label" htmlFor="search">Digite para buscar...</label>
+        </div>
+    </div>
+    );
     };
 }
 export default Home;
